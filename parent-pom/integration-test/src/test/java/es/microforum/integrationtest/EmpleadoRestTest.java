67dd7b0f9cbc635650f7a6c80dc25afee774490a
alumno <alumno@A09CLI02CD>
2014-02-07 22:14:04 +0100

    Actualizacion integration-test Añadido test get rest en el proyecto integration-test

---
+++ parent-pom/integration-test/src/test/java/es/microforum/integrationtest/EmpleadoRestTest.java
@@ -0,0 +1,36 @@
+package es.microforum.integrationtest;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+import java.net.URI;
+import org.junit.Test;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.hateoas.Resource;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.client.RestTemplate;
+
+import es.microforum.model.Empleado;
+
+public class EmpleadoRestTest {
+
+	RestTemplate restTemplate = new RestTemplate();
+
+	@Test
+	public void getTest() {
+		try {
+			Resource<Empleado> resource = getEmpleado(new URI("http://localhost:8081/service-frontend-0.0.3-SNAPSHOT/empleado/1"));
+			assertTrue(resource.getContent().getNombre().equals("nombre"));
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+
+	private Resource<Empleado> getEmpleado(URI uri) {
+		return restTemplate.exchange(uri, HttpMethod.GET, null, new ParameterizedTypeReference<Resource<Empleado>>() {
+		}).getBody();
+
+	}
+
+}
